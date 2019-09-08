92b08eeea3a6cd5f9530ef6e1ac776571f746378
Batyr Seven <batyrseven@gmail.com>
2019-09-08 14:53:53 +0300

    Transaction update function * improved cryptoHash function

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,6 +1,6 @@
 const Blockchain = require('.');
 const Block = require('./block');
-const crypyoHash = require('../util/crypto-hash');
+const { cryptoHash } = require('../util');
 
 describe('Blockchain', () => {
     let blockchain, newChain, originalChain;
@@ -68,7 +68,7 @@ describe('Blockchain', () => {
                     const data = [];
                     const difficulty = lastBlock.difficulty - 3;
 
-                    const hash = crypyoHash(timestamp, lastHash, difficulty, nonce, data);
+                    const hash = cryptoHash(timestamp, lastHash, difficulty, nonce, data);
 
                     const badBlock = new Block({
                         timestamp, lastHash, hash, nonce, difficulty, data
--- wallet/transaction.js
+++ wallet/transaction.js
@@ -28,6 +28,21 @@ class Transaction {
         return input;
     }
 
+    update({ senderWallet, recipient, amount  }) {
+        if (amount > this.outputMap[senderWallet.publicKey]) {
+            throw new Error('Amount exceeds balance');
+        }
+
+        if (this.outputMap[recipient]) {
+            this.outputMap[recipient] += amount;
+        } else {
+            this.outputMap[recipient] = amount;
+        }
+
+        this.outputMap[senderWallet.publicKey] -= amount;
+        this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
+    }
+
     static validate(transaction) {
         const { input: { address, amount, signature }, outputMap } = transaction;
 
