4222239d2d76af3fbbfa19579fda09b0e4e51695
Batyr Seven <batyrseven@gmail.com>
2020-04-30 16:28:00 +0300

    Clear recorded transactions on successful replace

--- blockchain/index.js
+++ blockchain/index.js
@@ -15,7 +15,7 @@ class Blockchain {
         this.chain.push(newBlock);
     }
 
-    replaceChain(chain) {
+    replaceChain(chain, onSuccess) {
         if (this.chain.length >= chain.length) {
             console.error('The incoming chain must be longer');
             return;
@@ -26,6 +26,8 @@ class Blockchain {
             return;
         }
 
+        if (onSuccess) onSuccess();
+        
         console.log('replacing chain with', chain);
         this.chain = chain;
     }
